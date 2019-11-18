1e950ff0f76d3f6aceb33f43fa613192abd73c73
COHRINT Lab <jamc3951@colorado.edu>
2019-11-18 11:39:13 -0700

    more

--- Modeling/Filtering/linear_KF.py
+++ Modeling/Filtering/linear_KF.py
@@ -14,11 +14,12 @@ def main():
 	#x = dynamics['initial_state']
 	#t = dynamics['timestep']
 
-	a = np.loadtxt('motion.txt', delimiter=',') #x,y,z
-	t = np.loadtxt('timestamp.txt', delimiter=', ')
+	a = np.loadtxt('stationary.txt', delimiter=',') #x,y,z
+	#t = np.loadtxt('timestamp.txt', delimiter=', ')
+	t = np.linspace(0,8,115)
 	w_noise = np.random.rand(1,6)*0.1
 	m_noise = np.random.rand(1,6)*0.1
-	ts = t[1,0]
+	ts = t[1]
 	position = np.array(a[0,:]*ts*ts)
 	velocity = np.array(a[0,:]*ts)
 	#B = [0.1 , 0 , 0 , 0]
@@ -35,35 +36,48 @@ def main():
 
 	H = np.eye(6)
 
-	R = np.eye(6)*100 #noise 0.1 rad
+	R = np.eye(6)*5 #noise 0.1 rad
 
 	pose, P = kf(x,t,P,A,Q,H,R,a,w_noise,m_noise)
 
 
 	fig, axs = plt.subplots(2, 2)
 
-	axs[0,0].plot(t[:,0],pose[:,0], color='blue')
+	axs[0,0].plot(t,pose[:,0], color='blue')
 	#plt.plot(t[:,0],P[:,0,0])
-	axs[0,1].plot(t[:,0],pose[:,1], color='blue')
-	axs[1,0].plot(t[:,0],pose[:,2], color='blue')
+	axs[0,1].plot(t,pose[:,1], color='blue')
+	axs[1,0].plot(t,pose[:,2], color='blue')
 
 
-	axs[0,0].plot(t[:,0],a[:,0], color='green')
-	axs[0,1].plot(t[:,0],a[:,1], color='green')
-	axs[1,0].plot(t[:,0],a[:,2], color='green')
+	axs[0,0].plot(t,a[:,0], color='green')
+	axs[0,1].plot(t,a[:,1], color='green')
+	axs[1,0].plot(t,a[:,2], color='green')
+
+	axs[1,1].plot(t,P[:,0,0], color='green')
+	axs[1,1].plot(t,P[:,1,1], color='blue')
+	axs[1,1].plot(t,P[:,2,2], color='red')
+
+	axs[0, 0].set_title('X')
+	axs[0, 1].set_title('Y')
+	axs[1, 0].set_title('Z')
+	axs[1, 1].set_title('P')
 
 	plt.xlabel('time (sec)')
 	plt.ylabel('m')
 
 
-	plt.legend(('x', 'y', 'z'))
-	plt.grid()
+	axs[1,1].legend(('x', 'y', 'z'))
+	axs[0,0].grid()
+	axs[0,1].grid()
+	axs[1,0].grid()
+	axs[1,1].grid()
+
 	plt.show()
 
 
 
 def kf(xp,t,Pp,A,Q,H,R,a, w,m): 
-	dt = t[1,0] # discrete
+	dt = t[1] # discrete
 	x = np.zeros((len(t),6))
 	x[0,:] = xp
 	P = np.zeros((len(t),6,6))
--- Modeling/Filtering/make_data.m
+++ Modeling/Filtering/make_data.m
@@ -1,11 +1,11 @@
 clear;close;clc;
 
-n = 1415;
+n = 115;
    
 for i = 1:n
-    x(i) =  10+rand*.1;
-    y(i) =  10+rand*.1;
-    z(i) =  10+rand*.1;
+    x(i) =  10+rand*1;
+    y(i) =  10+rand*1;
+    z(i) =  10+rand*1;
 end
 
 mat = [x' y' z'];
