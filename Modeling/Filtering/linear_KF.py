c6575c0e84f8fe65f283f9166a1ebc6106a7da4f
COHRINT Lab <jamc3951@colorado.edu>
2019-11-25 14:20:14 -0700

    Font size and legend

--- Modeling/Filtering/linear_KF.py
+++ Modeling/Filtering/linear_KF.py
@@ -24,46 +24,72 @@ def main():
 	velocity = np.array(a[0,:]*ts)
 	#B = [0.1 , 0 , 0 , 0]
 	x = [0,0,0,0,0,0]
-	P = np.eye(6)*np.random.rand(1,1) #Initial uncertainty
+	P = np.eye(6)*np.random.rand(1,1)*100 #Initial uncertainty
 	A = np.array([[1, 0, 0, ts, 0, 0],
                  [0, 1, 0, 0, ts, 0],
                  [0, 0, 1, 0, 0, ts],
                  [0, 0, 0, 1, 0, 0],
                  [0, 0, 0, 0, 1, 0],
                  [0, 0, 0, 0, 0, 1]])
-	Q = np.eye(6)*0.25 #actuator type uncertainy 1%
+	Q = np.eye(6)*0.75 #actuator type uncertainy 1%
 
 
 	H = np.eye(6)
 
-	R = np.eye(6)*5 #noise 0.1 rad
+	R = np.eye(6)*100 
 
 	pose, P = kf(x,t,P,A,Q,H,R,a,w_noise,m_noise)
 
+	stdx = np.sqrt(P[:,0,0])
+	stdy = np.sqrt(P[:,1,1])
+	stdz = np.sqrt(P[:,2,2])
+
+
 
 	fig, axs = plt.subplots(2, 2)
+	fig.suptitle("Kalman Filtered data for Beacon at (10,10,10)", fontsize=34)
+	#fig.legend('x', 'y', 'z')
+	#Truth data
+	truth = axs[0,0].hlines(10,0,8, linewidth = 2,linestyle = '--', color='black')
+	axs[0,1].hlines(10,0,8, linewidth = 2,linestyle = '--', color='black')
+	axs[1,0].hlines(10,0,8, linewidth = 2,linestyle = '--', color='black')
 
-	axs[0,0].plot(t,pose[:,0], color='blue')
+
+
+	filtered, = axs[0,0].plot(t,pose[:,0], color='blue')
 	#plt.plot(t[:,0],P[:,0,0])
-	axs[0,1].plot(t,pose[:,1], color='blue')
+	axs[0,1].plot(t,pose[:,1], color='blue', label='std')
 	axs[1,0].plot(t,pose[:,2], color='blue')
 
 
-	axs[0,0].plot(t,a[:,0], color='green')
+	raw, = axs[0,0].plot(t,a[:,0], color='green')
 	axs[0,1].plot(t,a[:,1], color='green')
 	axs[1,0].plot(t,a[:,2], color='green')
 
 	axs[1,1].plot(t,P[:,0,0], color='green')
 	axs[1,1].plot(t,P[:,1,1], color='blue')
 	axs[1,1].plot(t,P[:,2,2], color='red')
+	
+	#2 sigma bounds
+	sigma = axs[0,0].fill_between(t,stdx + pose[:,0],pose[:,0] - stdx, color='#CED8FF')
+	axs[0,1].fill_between(t,stdy + pose[:,1],pose[:,1] - stdx, color='#CED8FF')
+	axs[1,0].fill_between(t,stdz + pose[:,2],pose[:,2] - stdx, color='#CED8FF')
 
-	axs[0, 0].set_title('X')
-	axs[0, 1].set_title('Y')
-	axs[1, 0].set_title('Z')
-	axs[1, 1].set_title('P')
+	labels = ["Truth", "Raw", "Filtered", "sigma"]
+	fig.legend([truth, raw, filtered, sigma], labels, loc = (0.9, 0.5))
 
-	plt.xlabel('time (sec)')
-	plt.ylabel('m')
+	axs[0, 0].set_title('X',fontsize=24)
+	axs[0, 1].set_title('Y',fontsize=24)
+	axs[1, 0].set_title('Z',fontsize=24)
+	axs[1, 1].set_title('Covariance', fontsize=24)
+
+	axs[0,0].set_xlabel('time [s]', fontsize=20)
+	axs[0,0].set_ylabel('distance [m]', fontsize=20)
+	axs[0,1].set_xlabel('time [s]', fontsize=20)
+	axs[0,1].set_ylabel('distance [m]', fontsize=20)
+	axs[1,0].set_xlabel('time [s]', fontsize=20)
+	axs[1,0].set_ylabel('distance [m]', fontsize=20)
+	axs[1,1].set_xlabel('time [s]', fontsize=20)
 
 
 	axs[1,1].legend(('x', 'y', 'z'))
